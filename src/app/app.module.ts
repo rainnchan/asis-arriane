68e1b88fee8badb77939f4b87ec5b5ca106fb0e5
rainnchan <tinasprout213@gmail.com>
2023-01-28 17:31:07 +0800

    assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ewan-bc735',
+    appId: '1:557620045609:web:09f929f04ef0533206b938',
+    databaseURL: 'https://ewan-bc735-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'ewan-bc735.appspot.com',
+    apiKey: 'AIzaSyBcfgJK50qKI05EkIO5mhxiOXhDFiUA_WQ',
+    authDomain: 'ewan-bc735.firebaseapp.com',
+    messagingSenderId: '557620045609',
+    measurementId: 'G-E1CFTTFP2P',
+  },
   production: false
 };
 
