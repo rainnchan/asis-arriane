777daf6372d1d985f3a91f9a832148671be2831c
rainnchan <tinasprout213@gmail.com>
2023-03-03 18:32:17 +0800

    post component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PostComponent } from './post/post.component';
 import { ShowdataComponent } from './showdata/showdata.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path: 'signup',component:SignupComponent},
   {path: 'login',component:LoginComponent},
   {path: 'showdata',component:ShowdataComponent},
+  {path: 'post', component:PostComponent},
   {path: '',redirectTo:'signup',pathMatch:'full'}  
 
 ];
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {  AngularFireDatabase } from '@angular/fire/compat/database';
+import { Database,remove,ref,update, onValue, set} from '@angular/fire/database';
+
 
 @Component({
   selector: 'app-post',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
+  account!: Observable<any[]>;
+  constructor(public database: Database, private FireDb: AngularFireDatabase) {
+  this.account = FireDb.list('/post').valueChanges();}
 
   ngOnInit(): void {
   }
 
-}
+
+    postna(value:any){
+
+        
+      set(ref(this.database, 'post/' + value.name), {   
+          name: value.name,
+          post: value.post
+         }); 
+         alert('Posted!');
+        }
+       }
