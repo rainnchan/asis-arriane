2e0fbe59e932f1b96e78cb933c3cec4c8b90e29d
rainnchan <tinasprout213@gmail.com>
2023-03-21 17:10:40 +0800

    delete function on post added

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -10,30 +10,36 @@ import { Database,remove,ref,update, onValue, set} from '@angular/fire/database'
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-  data = "";
+  data = false;
   email = sessionStorage.getItem('email');
   account!: Observable<any[]>;
   constructor(public database: Database, private FireDb: AngularFireDatabase) {
   this.account = FireDb.list('/post').valueChanges();
-  // const starCountRef = ref(this.database, 'users/' + this.email);
-  // onValue(starCountRef, (snapshot) => {
-  //   const db = snapshot.val();
-  //   this.data = db.name;  
-  //   console.log(this.data)
-  //      });
+   const starCountRef = ref(this.database, 'users/' + this.email);
+   onValue(starCountRef, (snapshot) => {
+   const db = snapshot.val();
+   this.data = db.admin;  
+   console.log(this.data)
+     });
       }
   
-
+      del(value: any){
+        remove(ref(this.database, 'post/' + value));
+        alert('Deleted Successfully')
+      }
   ngOnInit(): void {
   }
 
-
+uid = "";
     postna(value:any){
+      console.log ("gumana")
+      this.uid = "post" +Math.floor(100000 + Math.random() * 900000);       
+      set(ref(this.database, 'post/' + this.uid), {   
 
-        
-      set(ref(this.database, 'post/' + value.name), {   
+
           name: value.name,
-          post: value.post
+          post: value.post,
+          id: this.uid
          }); 
          alert('Posted!');
         }
