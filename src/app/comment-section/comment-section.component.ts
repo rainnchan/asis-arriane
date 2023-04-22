e6d9ebb3062a264f853776c58142df6132d438c2
rainnchan <tinasprout213@gmail.com>
2023-04-22 19:12:56 +0800

    comment, reply,&reaction eated w/ no design yet

--- src/app/comment-section/comment-section.component.ts
+++ src/app/comment-section/comment-section.component.ts
@@ -21,6 +21,7 @@ addComment() {
 }
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -43,4 +43,93 @@ uid = "";
          }); 
          alert('Posted!');
         }
+
+        addComment(value:any){
+
+          this.uid = "comment" +Math.floor(100000 + Math.random() * 900000);
+          set(ref(this.database, 'post/'+value.id+'/comment/'+ this.uid), {
+            name: value.name1,
+            comment: value.post1,
+            id: this.uid,
+            postid: value.id
+          });
+     console.log(value.id)
+        }
+
+        comments!: Observable<any[]>;
+        currentpost = "";
+        modeC=false;
+        modeR=false;
+        likers = "";
+        getComment(post:any){
+          this.comments = this.FireDb.list('/post/'+post+'/comment/').valueChanges();
+          this.currentpost = post;
+          this.modeC=true;
+          this.modeR=false;
+          console.log("hello")
+          const starCountRef1 = ref(this.database, 'post/'+this.currentpost+'/like/'+ this.email);
+          onValue(starCountRef1, (snapshot) => {
+           const db1 = snapshot.val();  
+        this.likers = db1.name;
+              console.log(this.likers) 
+        
+           }); 
+       
+        }
+        currentcomment="";
+        rep(reply:any){
+          this.uid = "reply" +Math.floor(100000 + Math.random() * 900000);
+          set(ref(this.database, 'post/'+this.currentpost+'/comment/'+this.currentcomment+'/reply/'+this.uid), {   
+              name: reply.name1,
+              reply: reply.post1,
+              id: this.uid,
+              postid: this.currentpost,
+              commentid: this.currentcomment,         
+
+             }); 
+
+    
+         
+            this.modeC=true;
+            this.modeR=false;
+
+        }
+        reply!: Observable<any[]>;
+        getReply(reply:any){
+           this.currentcomment=reply 
+          this.reply = this.FireDb.list('/post/'+this.currentpost+'/comment/'+this.currentcomment+'/reply').valueChanges();
+   console.log("tesr")
+     
+         this.modeR=true;
+         this.modeC=false;
+         
+      }
+      likecounter=0;
+      like(value:any){
+        if(value == 0 || value == undefined || value == null){
+        update(ref(this.database, 'post/'+this.currentpost),{
+          likes:1
+          } );
+          set(ref(this.database, 'post/'+ this.currentpost +'/like/'+ this.email), {   
+            name: this.email
+           }); 
+    }else{
+      this.likecounter = value + 1;
+      update(ref(this.database, 'post/' + this.currentpost),{
+        likes:this.likecounter
+        } );
+        set(ref(this.database, 'post/'+ this.currentpost +'/like/'+ this.email), {   
+          name: this.email
+         }); 
+    }
+ }
+ //unliking a post
+ unlike(value:any){
+   remove(ref(this.database, '/post/'+ this.currentpost +'/like/'+ this.email));
+   this.likecounter = value - 1;
+   update(ref(this.database, 'post/' + this.currentpost),{
+     likes:this.likecounter
+     } );
+     this.likers=""
+ }
        }
